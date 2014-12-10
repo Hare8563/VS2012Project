6261358dd3d8aa409e0068d281028cf4811d757d
Hare8563 <haresaku1024@gmail.com>
2014-12-10 13:59:13 +0900

    Update VS2012

---
+++ IRUProject1/FaceRecognition/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FaceRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(176, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 371);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "cascadeSelection";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(424, 19);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 74);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 443);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "FaceRecognition";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ IRUProject1/FaceRecognition/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+
+namespace FaceRecognition
+{
+    public partial class Form1 : Form
+    {
+
+        IplImage img;
+        Dictionary<string, string> cascadeDictionary = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] files = System.IO.Directory.GetFiles(@"C:\dev\2.4.6\opencv\data\haarcascades", "*.xml", System.IO.SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                comboBox1.Items.Add(file);
+            }
+
+
+            for (int i = 0; i < comboBox1.Items.Count;i++)
+                cascadeDictionary.Add(comboBox1.Items[i].ToString(), @"C:\dev\2.4.6\opencv\data\haarcascades\" + comboBox1.Items[i].ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //はじめのファイル名を指定する
+
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "Imageファイル(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png";
+            //[ファイルの種類]ではじめに
+            //「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                //選択されたファイル名を表示する
+                textBox1.Text = ofd.FileName;
+                img = Cv.LoadImage(ofd.FileName, LoadMode.Color);
+                LoadImage(img.ToBitmap());
+            }
+        }
+
+        private void LoadImage(Bitmap bmp)
+        {
+            double hi;
+            double imagew = bmp.Width;
+            double imageh = bmp.Height;
+
+            if ((pictureBox1.Height / pictureBox1.Width) <= (imageh / imagew))
+            {
+                hi = pictureBox1.Height / imageh;
+            }
+            else
+            {
+                hi = pictureBox1.Width / imagew;
+            }
+            int w = (int)(imagew * hi);
+            int h = (int)(imageh * hi);
+
+            Bitmap result = new Bitmap(w, h);
+            Graphics g = Graphics.FromImage(result);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(bmp, 0, 0, result.Width, result.Height);
+
+            pictureBox1.Image = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            const double ScaleFactor = 1.0850;
+            const int MinNeighbors = 2;
+
+
+            if (comboBox1.SelectedIndex < 0 || img == null)
+            {
+                MessageBox.Show(@"cascadeの選択、またはファイルを選択してください");
+                return;
+            }
+
+            IplImage grayImg = new IplImage(img.Width, img.Height, BitDepth.U8, 1);
+            Cv.CvtColor(img, grayImg, ColorConversion.BgrToGray);
+
+            //String cascadeName = cascadeDictionary[comboBox1.SelectedItem];
+            //CascadeClassifier cascadeClassifier = new CascadeClassifier(cascadeName);
+            CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile(comboBox1.SelectedItem.ToString());
+            CvMemStorage storage = new CvMemStorage();
+            storage.Clear();
+
+            CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(grayImg, cascade, storage, ScaleFactor, MinNeighbors, 0, new CvSize(30, 30));
+            
+            
+            // 検出した箇所に四角をつける
+            for (int i = 0; i < faces.Total; i++)
+            {
+                CvRect r = faces[i].Value.Rect;
+                img.Rectangle(r, new CvColor(255, 0, 0));
+            }
+
+            LoadImage(img.ToBitmap());
+        }
+
+
+
+    }
+}
